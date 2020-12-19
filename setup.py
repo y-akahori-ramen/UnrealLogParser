5a9e366e8d40ae057f1a06c9ca9bd423ec60081e
Yuki Akahori <y.akahori.ramen@gmail.com>
2020-12-19 16:06:51 +0900

    リリースに向けてsetuptoolsの情報追加

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='uelogparser',
+    version='1.0.0',
+    author='y-akahori-ramen',
+    url='https://github.com/y-akahori-ramen/UnrealLogParser',
+    description='Parsing UnrealEngine Logs in Python',
+    python_requires='~=3.7',
     packages=find_packages(),
 )
